62398f39d960a14870cd4812ad3821b12fb6cbf6
Kyle Millar <kyledmillar@gmail.com>
2024-06-17 18:42:48 +0100

    Add user interface

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 let getComputerChoice = () => {
     let random = Math.random();
     if (random <= 1/3) {
@@ -9,40 +12,33 @@ let getComputerChoice = () => {
     }
 }
 
-let getHumanChoice = () => {
-    let choice = prompt("Choose rock, paper or scissors: ");
-    return choice;
+let playRound = (humanChoice, computerChoice) => {
+    const scoreDiv = document.querySelector('div');
+    console.log(humanChoice)
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    if (humanChoice == "rock" && computerChoice == "scissors"
+        || humanChoice == "paper" && computerChoice == "rock"
+        || humanChoice == "scissors" && computerChoice == "paper") {
+        console.log(`You win, ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    }
+    else if (computerChoice == "rock" && humanChoice == "scissors"
+        || computerChoice == "paper" && humanChoice == "rock"
+        || computerChoice == "scissors" && humanChoice == "paper") {
+        console.log(`You lose, ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+    else {
+        console.log(`It's a draw, you both chose ${humanChoice}`);
+    }
+    scoreDiv.textContent = `${humanScore} - ${computerScore}`;
 }
 
-let playGame = () => {
-    let playRound = (humanChoice, computerChoice) => {
-        console.log(humanChoice)
-        humanChoice = humanChoice.toLowerCase();
-        computerChoice = computerChoice.toLowerCase();
-        if (humanChoice == "rock" && computerChoice == "scissors"
-            || humanChoice == "paper" && computerChoice == "rock"
-            || humanChoice == "scissors" && computerChoice == "paper") {
-            console.log(`You win, ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
-        }
-        else if (computerChoice == "rock" && humanChoice == "scissors"
-            || computerChoice == "paper" && humanChoice == "rock"
-            || computerChoice == "scissors" && humanChoice == "paper") {
-            console.log(`You lose, ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }
-        else {
-            console.log(`It's a draw, you both chose ${humanChoice}`);
-        }
-    }
-    let humanScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-    console.log(`Final score: you ${humanScore}, computer ${computerScore}`);
-}
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
 
-playGame();
+rockButton.addEventListener('click', () => playRound('rock', getComputerChoice()));
+paperButton.addEventListener('click', () => playRound('paper', getComputerChoice()));
+scissorsButton.addEventListener('click', () => playRound('scissors', getComputerChoice()));
